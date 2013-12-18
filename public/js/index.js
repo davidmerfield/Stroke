e5ddf3d6d36e53abd2c85b219b7051379643ee9f
David Merfield <dmerfield@gmail.com>
2013-12-18 11:51:08 -0800

    basic working prototype

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,7 @@
-; (function($) {
+// tab breaks this
+
+
+(function($) {
     $.fn.focusToEnd = function() {
         return this.each(function() {
             var v = $(this).val();
@@ -7,177 +10,133 @@
     };
 })(jQuery);
 
-function setEndOfContenteditable(contentEditableElement)
-{
-    var range,selection;
-    if(document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
-    {
-        range = document.createRange();//Create a range (a range is a like the selection but invisible)
-        range.selectNodeContents(contentEditableElement);//Select the entire contents of the element with the range
-        range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
-        selection = window.getSelection();//get the selection object (allows you to change selection)
-        selection.removeAllRanges();//remove any selections already made
-        selection.addRange(range);//make the range you have just created the visible selection
+function selectAll(el) {
+    if (typeof window.getSelection != "undefined" && typeof document.createRange != "undefined") {
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (typeof document.selection != "undefined" && typeof document.body.createTextRange != "undefined") {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(el);
+        textRange.select();
     }
-    else if(document.selection)//IE 8 and lower
-    { 
-        range = document.body.createTextRange();//Create a range (a range is a like the selection but invisible)
-        range.moveToElementText(contentEditableElement);//Select the entire contents of the element with the range
-        range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
-        range.select();//Select the range (make it the visible selection
+}
+
+function getSelectionHtml() {
+    var html = "";
+    if (typeof window.getSelection != "undefined") {
+        var sel = window.getSelection();
+        if (sel.rangeCount) {
+            var container = document.createElement("div");
+            for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+                container.appendChild(sel.getRangeAt(i).cloneContents());
+            }
+            html = container.innerHTML;
+        }
+    } else if (typeof document.selection != "undefined") {
+        if (document.selection.type == "Text") {
+            html = document.selection.createRange().htmlText;
+        }
     }
+    return(html);
 }
 
 $(function() {
 
-    // populate();
+    //Sample sound table
+    var sounds = {"1":new Audio("/public/audio/1.mp3"),
+                  "2":new Audio("/public/audio/2.mp3"),
+                  "3":new Audio("/public/audio/3.mp3"),
+                  "4":new Audio("/public/audio/4.mp3"),
+                  "5":new Audio("/public/audio/5.mp3"),
+              };
 
-    function populate () {
-        var foo = '',
-            rows = 2,
 
-        for (var i = 0;i < rows; i++){
-          foo+= '1 ';
-        }
-
-        for (var i = 0;i < rows; i++){
-          foo+= '0';
-        }
-
-        // for (var i = 0;i < rows*cols; i++){
-        //   foo+= 'new line \n';
-        // }
-
-        $('#input').html(foo)
+    //Resets a sound's timer and plays it from the start
+    function playSound(){
+    var name = Math.floor(Math.random()*6).toString();
+    sounds[name].currentTime = 0;
+    sounds[name].play();
     }
-
-    function strike(position, content) {
-        
-
-        console.log('called');
-
-        var cols = parseInt($('#input').attr('cols')) + 1,
-            lineHeight = 36,
-            characterWidth = 14;
-
-            positionY = lineHeight*Math.floor(position/cols),
-            positionX = characterWidth*(position%cols);
-
-        var position = position + 1;
-
-        console.log('position ' + position);
-        console.log('positionX: ' + positionX);
-
-        // for (var i = 0; i < positionX;i++){
-        //     content = '\u00a0' + content;
-        //     console.log(content);
-        // };
-
-        var html = '<textarea style="top:' + positionY + 'px;text-indent:' + positionX +  'px">' + content + '</textarea>';
-
-            console.log('position: ' + position);
-            console.log('cols: ' + cols);
-            console.log('column offset: ' + position%cols);
-            
-        $('#strikes').append(html);
-
-    }
+                        
 
 
-  $('#helper').click(function(e){
-    e.preventDefault();
+    $(window)
+        .click(function(){
+            window.setTimeout(function(){
+                if (!getSelectionHtml()) {
+                    $('#input').focus();
+                };
+            }, 150)
+        });
 
-    console.log($('#output').selection());
-    console.log($.selection('html'));
-    return false
-  });
+    $('#audio_toggle').click(function(){
+        $(this).toggleClass('enabled');
+    });
+
+    $('#lights').click(function(){
+        $('body').toggleClass('dark');
+    });
+    
+    $('#select').click(function(){
+        selectAll($('#output')[0]);
+    });
 
 
+$('body')
+    .keydown(function(e){
+        var keyCode = e.which;
+        // Delete
 
-    function replaceSpace(){
 
-        if ($('#input').selection()) {
-            
-            console.log($('#input').selection());
-
-            var selectedText = $('#input').selection(),
-                replacement = selectedText.replace(/\S/g, '\u00a0'),
-                position = $('#input').selection('getPos').start;
-
-            console.log(position);
-
-            $('#input').selection('replace', {text: replacement, caret: 'keep'});
-
-            strike(position, selectedText);
-
+        // Disable arrow keys
+        if (keyCode <= 40 && keyCode >= 37) {
+             e.preventDefault();             
         }
 
-        return setTimeout(function() {
+        if (keyCode === 13) {
+             $('#input').val($('#input').val() + '<br />')
+        }
+
+        if (keyCode === 8) {
+            
+            console.log('this');
+
+            // Don't delete anything, you can't delete words on a typewriter
+            e.preventDefault();             
+
+            // Strike out the text that would be deleted
+            var strike = document.createElement('strike'),
+
+            // Get the contents of 
+                selectedText = window.getSelection().getRangeAt();
+
+            console.log(selectedText);
+
+            selectedText.surroundContents(strike);
+            
+            // Reflect strike in source textarea
+            $('#input').val($('#output').html())
+
+            // Move the caret to the end of the contenteditable
+            // so the user can keep typing fluidly
             $('#input').focusToEnd();
-        }, 0);            
-
-    }
+        };
+    });
 
   $('#input')
-    .keydown(function(e) {
+    .focus()
+    .keyup(function(e){
+        $('#output').html($('#input').val());
+    })
+    .keypress(function(e) {        
         
-        if ($('#input').selection() && e.which !== 8){
-            e.preventDefault();
-            return false
+        if ($('#audio_toggle').hasClass('enabled')){
+            playSound();            
         }
-
-        if (e.which <= 40 &&
-            e.which >= 37) {
-            e.preventDefault();             
-        }
-
-        if (e.which === 13) {
-            
-            e.preventDefault();             
-
-            var val = $("#input").val(),
-                position = $("#input").selection('getPos').start + 1,
-                cols = parseInt($('#input').attr('cols')) + 1,
-                replacementLength = cols - (position)%cols,
-                replacement = '';
-
-            if (position === 0) {replacementLength = cols};
-
-            console.log('replacementLength: ' + replacementLength);
-            console.log('position: ' + position);
-
-            while (replacement.length < replacementLength) {
-                replacement += ' ';
-            }
-
-            $("#input").val(val + replacement + '\n');
-
-        }
-
-        if (document.URL.slice(-1) === "/") {
-
-            $("#itch").fadeOut();
-
-            var slug = Math.random().toString(36).slice(2, 8);
-            
-            $.get('/new', {just_the_slug: true}, function(slug, textStatus) {
-                // console.log(slug);
-                // console.log(textStatus);
-                window.history.pushState("", "Typewriter", "/" + slug);  
-            });
-        };
-
-        // $.post('/update', {slug: document.URL.slice(-6), contents: $(this).val()}, function(data, textStatus) {
-        //     // console.log(data);
-        //     // console.log(textStatus);
-        // });
-
-        // Disable backspace
-        if (e.which === 8) {
-            
-            e.preventDefault();             
-            replaceSpace();
-        };
-
+        
+        $('#output').append(String.fromCharCode(e.which));
     });
 });
