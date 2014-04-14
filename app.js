c343b1b01fc434bba3a1274b058385d02f513097
David Merfield <dmerfield@gmail.com>
2014-04-14 12:51:18 -0700

    added strike out across multiple lines of text

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ Typewriter.init = function() {
 
     // This is to get around the weird highlighting
     // issues for contenteditable
-    $('#input').append('<div id="start"><br></div>');
+
+    var firstDiv = document.createElement('div');
+
+    firstDiv.innerHTML = '&#8203;';
+
+    var input = document.getElementById('input');
+    input.appendChild(firstDiv);
 
     // Initiate
     Typewriter.sync('output', 'input');
@@ -15,11 +21,8 @@ Typewriter.init = function() {
 
 Typewriter.sync = function(oldNodeID, newNodeID) {
     
-    console.log
     var newNode = document.getElementById(newNodeID),
-        oldNode = document.getElementById(oldNodeID)
-    
-    ;
+        oldNode = document.getElementById(oldNodeID);
 
     // Update #output to reflect the changes made to #input
     return oldNode.innerHTML = newNode.innerHTML
@@ -29,46 +32,131 @@ $(function() {
 
     Typewriter.init();
 
+    // Keep focus on input no matter what
     $(window).click(function(){
         window.setTimeout(function(){
             if (!util.getSelectionHtml()) {
-                util.setEndOfContenteditable($('#input')[0]);
+                util.setEndOfContenteditable('input');
             };
         }, 150)
     });
 
+    // Typing Handler
     $('body').keydown(function(e){
         
         var keyCode = e.which;
 
-        // Disable arrow keys
+        // ARROW KEYS
         if (keyCode <= 40 && keyCode >= 37) {
-             e.preventDefault();             
+             e.preventDefault(); // disable them
         };
 
+        // ENTER
         if (keyCode === 13) {
-            $('#input').append('<div><br></div>');
+            $('#input').append('<div><br></div>'); // make new paragraph
             e.preventDefault();
         };
 
+        // DELETE
         if (keyCode === 8) {
                 
-            // Don't delete anything, you can't delete words on a typewriter
+            // Don't delete any characters
             e.preventDefault();             
 
-            // Strike out the text that would be deleted
-            var strike = document.createElement('span'),
-                span = document.createElement('span'),
-                selectedText = window.getSelection().getRangeAt();
-            
-            strike.className = 'strikeout';
-            span.innerHTML = '&#8203;';
+            // Check for selected text to strike out
 
-            console.log(selectedText);
+            var selectedText = window.getSelection().getRangeAt();
 
-            selectedText.surroundContents(strike);
-            selectedText.collapse(false);
-            selectedText.insertNode(span);
+            // If selected text contains non text nodes then strikeout will not work
+            if (selectedText.commonAncestorContainer.nodeName === '#text'){
+                strikeOut(selectedText)
+            } else {
+
+                var textRanges = splitUp(selectedText),
+                    textRangeLen = textRanges.length;
+
+                for (var i = 0;i<textRangeLen;i++){
+                    strikeOut(textRanges[i]);
+                }
+                
+            }
+
+            function strikeOut(range) {
+
+                // The black line through the text
+                var strike = document.createElement('span');
+                    strike.className = 'strikeOut';
+
+                range.surroundContents(strike);
+
+                // The 'empty' span is to counter
+                // weird behaviour when setting focus of contenteditable
+                // Without it, you can keep typing within strike out text
+                var emptySpan = document.createElement('span');
+                    emptySpan.innerHTML = '&#8203;'; // zero width character
+
+                // Adds empty span after strike
+                range.collapse(false);
+                range.insertNode(emptySpan);
+
+            }
+
+            // The purpose of this function is to take a range which includes non text nodes and
+            // split it into a series of ranges whose common ancestor is a text node
+            // then pass these ranges to strikeout()
+
+            function splitUp(range) {
+
+                var output = [];
+
+                console.log(range);
+
+                var selEms = util.getSelectedElementTags(window);
+                var textNodes = [];
+
+                for (var i = 0;i<selEms.length;i++) {
+                    if (selEms[i].id !== 'output' &&
+                        selEms[i].nodeName !== 'BR') {
+                        textNodes.push(selEms[i])
+                    }
+                }
+
+                for (var i = 0;i<textNodes.length;i++) {
+                    
+                    var node = textNodes[i];
+
+                    for (var key in node.childNodes) {
+
+
+                        var text = node.childNodes[key];
+
+                        console.log(text);
+
+                        if (text.nodeName === '#text') {
+
+                            var subRange = document.createRange();
+
+                            subRange.selectNode(text)                                
+
+                            if (text.isSameNode(range.startContainer)) {
+                                subRange.setStart(text, range.startOffset);
+                                // if this doesn't work check that node is added to the range
+                            }
+
+                            else if (text.isSameNode(range.endContainer)) {
+                                subRange.setEnd(text, range.endOffset);
+                            } 
+
+                            output.push(subRange);
+
+                        }
+                    };
+
+                }
+
+                return output
+
+            }
 
             // Reflect strike in source textarea
              Typewriter.sync('input', 'output');
@@ -88,8 +176,59 @@ $(function() {
 
 });
 
+
+
 function loadUtilities () {
     return {
+        rangeIntersectsNode: function(range, node) {
+            var nodeRange;
+            if (range.intersectsNode) {
+                return range.intersectsNode(node);
+            } else {
+                nodeRange = node.ownerDocument.createRange();
+                try {
+                    nodeRange.selectNode(node);
+                } catch (e) {
+                    nodeRange.selectNodeContents(node);
+                }
+
+                return range.compareBoundaryPoints(Range.END_TO_START, nodeRange) == -1 &&
+                    range.compareBoundaryPoints(Range.START_TO_END, nodeRange) == 1;
+            }
+        },
+
+        getSelectedElementTags: function(win) {
+
+            var range, sel, elmlist, treeWalker, containerElement;
+
+            sel = win.getSelection();
+            if (sel.rangeCount > 0) {
+                range = sel.getRangeAt(0);
+            }
+
+            if (range) {
+                containerElement = range.commonAncestorContainer;
+                if (containerElement.nodeType != 1) {
+                    containerElement = containerElement.parentNode;
+                }
+
+                treeWalker = win.document.createTreeWalker(
+                    containerElement,
+                    NodeFilter.SHOW_ELEMENT,
+                    function(node) { return util.rangeIntersectsNode(range, node) ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_REJECT; },
+                    false
+                );
+
+                elmlist = [treeWalker.currentNode];
+
+                while (treeWalker.nextNode()) {
+                    elmlist.push(treeWalker.currentNode);
+                }
+
+                return elmlist;
+            }
+        },
+
         setEndOfContenteditable: function(elId) {
             var range,
                 selection;
