cf15f7f688bebab31485b20f10b2095c2783eed8
David Merfield <dmerfield@gmail.com>
2014-04-24 21:51:20 -0700

    refactored code and moved it to unified namespace

---
+++ src/typewriter.js
@@ -0,0 +1,195 @@
+var typewriter = function () {
+
+   var input = document.getElementById('input'),
+       output = document.getElementById('output');
+   
+   function buildGUIMenu () {
+      return
+   }
+
+   // Used to check if the user has selected text
+   function selectedText() {
+      var selection = window.getSelection();
+      return selection.type === 'Range' ? selection : false
+   }
+
+   function sync (oldNode, newNode) {
+      return oldNode.innerHTML = newNode.innerHTML
+   }
+
+   function inDesktop () {
+      return typeof require !== 'undefined' ? true : false
+   }
+
+   function isArrowKey (keyCode) {
+      return (keyCode <= 40 && keyCode >= 37) ? true : false
+   }
+
+   function moveViewportToBottom () {
+      window.scrollTo(0, document.body.offsetHeight);
+   }   
+
+   function strikeOut() {
+
+      var selectedText = window.getSelection().getRangeAt();
+
+      console.log(window.getSelection());
+      console.log(window.getSelection().getRangeAt());
+
+      // The black line through the text
+      var strike = document.createElement('span');
+          strike.className = 'strike';
+
+      selectedText.surroundContents(strike);
+
+      // The 'empty' span is to counter
+      // weird behaviour when setting focus of contenteditable
+      // Without it, you can keep typing within strike out text
+      var emptySpan = document.createElement('span');
+          emptySpan.innerHTML = '&#8203;'; // zero width character
+
+      // Adds empty span after strike
+      selectedText.collapse(false);
+      selectedText.insertNode(emptySpan);
+
+   }
+
+   function setFocus (el) {
+      // Ensure el is contenteditable
+      el.setAttribute("contentEditable", true);
+
+      var range = document.createRange();
+          range.selectNodeContents(el);
+          range.collapse(false);
+      
+      var selection = window.getSelection();
+          selection.removeAllRanges();
+          selection.addRange(range);
+   }
+
+   document.onkeydown = function (e) {
+      if (e.which === 8) {
+         e.preventDefault();
+         strikeOut();
+         sync(input, output)
+         return setFocus(input);         
+      }
+   }
+   output.onkeydown = function (e) {      
+      
+      var keyCode = e.which;
+
+      if (keyCode === 16) {
+         return
+      }
+      
+      if (!isArrowKey(keyCode) && e.shiftKey) {
+         e.preventDefault();
+         return setFocus(input)
+      }
+
+      if (isArrowKey(keyCode) && !e.shiftKey) {
+         return setFocus(input)
+      }
+
+      if (isArrowKey(keyCode) && e.shiftKey) {
+         return ''
+      }
+
+      if (e.metaKey) {
+         return setFocus(input)
+      }
+
+      if (selectedText()) {
+         e.preventDefault()
+      }
+
+      return setFocus(input)
+
+   }
+
+   // We never want to see a cursor
+   output.onblur = function (e) {
+      output.setAttribute("contentEditable", false);
+   }
+
+   output.onkeyup = function (e) {
+      if (!selectedText()) {
+         return setFocus(input)
+      }
+   };
+
+   input.onkeydown = function (e) {      
+      
+      var keyCode = e.which;
+
+      // Disable delete key
+      if (keyCode === 8) {
+         e.preventDefault()
+      };
+
+      // Disable copy paste and shit
+      if (e.metaKey && keyCode !== 82) {
+         e.preventDefault()
+      }
+
+      // Handle arrow keys
+      if (keyCode <= 40 && keyCode >= 37) {
+         
+         // If shift is held allow user to make selection in output
+         if (e.shiftKey) {
+            return setFocus(output);
+
+         // Otherwise disable the arrow keys
+         } else {
+            e.preventDefault();            
+         }
+
+      };
+
+      // Handle return key
+      if (keyCode === 13) {
+         input.innerHTML += '<p></p>&#xfeff;'; // zero width char to fix contenteditable focus bug
+         e.preventDefault();
+      };
+
+      // We make a setTImeout because
+      // input.innerHTML is not yet updated with e.char
+      // and waiting for onkeypress is too slow
+      return setTimeout(function(){
+
+         sync(output, input);
+         setFocus(input);
+         return moveViewportToBottom();
+
+      }, 0);
+
+   }
+
+   document.onmouseup = function (e) {
+      
+      // We need to keep focus on input unless there's selected text
+      if (!selectedText()) {
+         setFocus(input);
+      }
+
+      // Since theres selected text,
+      // perhaps prepare for strikeout?
+
+   }
+
+   // This starts the typewriter
+   return function init () {
+      if (inDesktop()) {
+         console.log('Running in desktop environment!')
+      } 
+
+      sync(output, input);
+
+      setFocus(input);
+   }();
+
+}
+
+// GO GO GADGET
+typewriter();
