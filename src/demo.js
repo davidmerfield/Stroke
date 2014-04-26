fec9d359f0bdeed21e15b5682dfd0d15a91de818
David Merfield <dmerfield@gmail.com>
2014-04-26 12:04:18 -0700

    added strike function to demo

--- src/demo.js
+++ src/demo.js
@@ -22,6 +22,71 @@ var demo = function (el) {
       }, Math.floor(Math.random()*200)); 
    }
 
+   function crossOut(count, callback) {
+
+      var i = 1;
+
+      var lastP = el.lastChild,
+          lastPText = lastP.firstChild;
+
+      selectChars(count);
+
+      function selectChars(count) {
+
+         if (count === i) {return callback()};
+
+         var range = document.createRange();
+
+         range.selectNode(lastPText);
+         range.setStart(lastPText, lastPText.length - i);
+
+         var selection = window.getSelection();
+
+         selection.removeAllRanges();
+         selection.addRange(range);
+
+         i++;
+
+         setTimeout(function(){
+            selectChars(count);
+         }, Math.floor(Math.random()*200) + 100); 
+
+      }
+   }
+
+   function strike(callback) {
+      var selectedText = window.getSelection();
+      var range = selectedText.getRangeAt();
+
+      var strike = document.createElement('span');
+          strike.className = 'strike';
+
+      range.surroundContents(strike);
+
+      // The 'empty' span is to stop the user typing within strike out text
+      var emptySpan = document.createElement('span');
+          emptySpan.innerHTML = '&#8203;'; // zero width character
+
+      // Adds empty span after strike
+      range.collapse(false);
+      range.insertNode(emptySpan);         
+   
+      setFocus(document.getElementById('input'));
+
+      return callback()
+   }
+
+   function setFocus (el) {
+
+      var range = document.createRange();
+          range.selectNodeContents(el);
+          range.collapse(false);
+      
+      var selection = window.getSelection();
+          selection.removeAllRanges();
+          selection.addRange(range);
+   }
+
    function newLine (callback) {
 
       setTimeout(function(){
