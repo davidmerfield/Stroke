0fb5e1608b81a70d5d8d4d17ec53c379119c11a0
David Merfield <dmerfield@gmail.com>
2014-04-27 00:07:13 -0700

    moved demo files

--- homepage/demo.js
+++ homepage/demo.js
@@ -1,22 +1,15 @@
-var demo = function (el) {
+var demo = function () {
    
-   el = document.getElementById(el);
+   el = document.getElementById('demo');
+
    input = document.getElementById('input');
    output = document.getElementById('output');
-
-   function setHTMLof (oldNode) {
-      return {
-         to: function(newNode) {
-            return oldNode.innerHTML = newNode.innerHTML            
-         }
-      }
-   };
 
    function type(string, callback, p) {
       
       if (!p) {
          p = document.createElement('p');
-         el.appendChild(p);
+         output.appendChild(p);
       }
 
       if (string === '') {
@@ -36,14 +29,18 @@ var demo = function (el) {
 
       var i = 1;
 
-      var lastP = el.lastChild,
+      var lastP = output.lastChild,
           lastPText = lastP.firstChild;
 
       selectChars(count);
 
       function selectChars(count) {
 
-         if (count === i) {return callback()};
+         if (count === i) {
+            var selectedText = window.getSelection();
+            typewriter().strikeOut(selectedText);
+            return callback()
+         };
 
          var range = document.createRange();
 
@@ -64,46 +61,13 @@ var demo = function (el) {
       }
    }
 
-   function strike(callback) {
-      var selectedText = window.getSelection();
-      var range = selectedText.getRangeAt();
-
-      var strike = document.createElement('span');
-          strike.className = 'strike';
-
-      range.surroundContents(strike);
-
-      // The 'empty' span is to stop the user typing within strike out text
-      var emptySpan = document.createElement('span');
-          emptySpan.innerHTML = '&#8203;'; // zero width character
-
-      // Adds empty span after strike
-      range.collapse(false);
-      range.insertNode(emptySpan);         
-   
-      setFocus(document.getElementById('input'));
-
-      return callback()
-   }
-
-   function setFocus (el) {
-
-      var range = document.createRange();
-          range.selectNodeContents(el);
-          range.collapse(false);
-      
-      var selection = window.getSelection();
-          selection.removeAllRanges();
-          selection.addRange(range);
-   }
-
    function newLine (callback) {
 
       setTimeout(function(){
 
          p = document.createElement('p');
          p.innerHTML = '&#xfeff;';
-         el.appendChild(p);
+         output.appendChild(p);
 
          setTimeout(function(){
             return callback()
@@ -115,34 +79,37 @@ var demo = function (el) {
    return function () {
       // input.onkeydown = function(e){return e.preventDefault()};
 
-      output.innerHTML = '';
+      typewriter().init();
 
-      type('Typewrite is a simple text editor.', function(){
+      output.innerHTML = '';
+      input.innerHTML = '';
+
+
+      input.setAttribute('style', 'display:none');
+      type('Typewriter is a simple text editor.', function(){
 
       newLine(function(){
       
-      type(' Every letter you type is permanent. No editing. No formatting. A little bit like a typewriter.', function(){
+      type(' Every letter you type is permanent. No editing. No formatting. A bit like a real typewriter.', function(){
 
       newLine(function(){
       
       type('Deleting text just strikes it out, so you can cover up mistskes', function(){
 
       crossOut(9, function(){
-      
-      strike(function(){
-      
+            
       type(' mistakes.', function(){    
 
       newLine(function(){
       
       type('Typewrite is perfect for first drafts. For forcing your ideas onto the page. For fixing writer\'s block.', function(){
       
-      setHTMLof(input).to(output)
-                           
+      typewriter().setHTMLof(input).to(output)
+            
+            input.setAttribute('style', 'display:block');               
       });
       });
-      }, el.lastChild);          
-      });
+      }, output.lastChild);          
       });
       });
       });
@@ -153,5 +120,5 @@ var demo = function (el) {
 }
 
 setTimeout(function(){
-   demo('output');
+   demo();
 }, 300);
