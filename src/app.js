d4adae10f478bbe87fe613860b006e7c531e8768
David Merfield <dmerfield@gmail.com>
2014-04-28 12:50:03 -0700

    open file now checks for existing window

--- src/app.js
+++ src/app.js
@@ -1,165 +1,200 @@
 var app = function () {
 
-   var input = document.getElementById('input'), // captures the user's text input
-       output = document.getElementById('output'); // renders the user's text input
+  var gui = require('nw.gui'),
+      fs = require('fs'),
 
-    var gui = require('nw.gui'),
-        fs = require('fs'),
-        windowPrefs = {
-          position: 'mouse',
-          title: 'Untitled',
-          width: 712,
-          height: 820,
-          show: false,
-          frame: true,
-          focus: false,
-          toolbar: false
-        };
+      input = document.getElementById('input'), // captures the user's text input
+      output = document.getElementById('output'), // renders the user's text input
 
-   var fileName= undefined;
-       filePath = undefined;
+      currentWindow = gui.Window.get(),
 
-    document.onkeydown = function (e) {
-        
-        var keyCode = e.which;
+      windowView = 'index.html',
+      windowPrefs = {
+        position: 'mouse',
+        title: 'Untitled',
+        width: 712,
+        height: 820,
+        show: false,
+        focus: false,
+        frame: true,
+        toolbar: false,
+        min_width: 300,
+        min_height: 200
+      };
 
-        if (keyCode === 78 && e.metaKey) {
-            newFile();
-        };
+  document.fileName = undefined;
+  document.filePath = undefined;
 
-        if (keyCode === 83 && e.metaKey) {
-            saveFile();
-        };
+  // Listen for app keyboard shortcuts
+  document.onkeydown = function (e) {
+      
+      var keyCode = e.which;
 
-        if (keyCode === 79 && e.metaKey) {
-            openFile();
-        };
+      // CMD + N
+      if (keyCode === 78 && e.metaKey) {
+          newFile();
+      };
+
+      // CMD + S
+      if (keyCode === 83 && e.metaKey) {
+          saveFile();
+      };
+
+      // CMD + O
+      if (keyCode === 79 && e.metaKey) {
+          openFile();
+      };
+  };
+
+  function newFile (openFile) {
+
+    var newX = currentWindow.x + 32,
+        newY = currentWindow.y + 32;
+        url = windowView + '?x=' + newX + '&y=' + newY;
+
+    if (openFile) {
+       url += '&openFile=true' // Tells the new window to open the select file interface
+    }
+
+    gui.Window.open(url, windowPrefs);
+  }
+
+  function openFile () {
+     newFile(true);
+  }
+
+  function closeFile () {
+    if (document.filePath !== undefined || output.innerHTML === '') {
+      return this.close(true);  
+    }
+    
+    var reallyClose = window.confirm("You have not yet saved your work. Do you still want to close this document?");
+    
+    if (reallyClose) {
+      currentWindow.close(true);          
+    } else {
+      saveFile();
     };
 
-    function buildMenu () {
-        var menubar = new gui.Menu({ type: 'menubar' });
-        var file = new gui.Menu();
-        var win = gui.Window.get();
+  }
 
-        file.append(new gui.MenuItem({
-            label: 'New             ⌘N',
-            click: function() {
-            return newFile()
-            }
-        }));
+  function saveFile (callback) {
 
-        file.append(new gui.MenuItem({
-            label: 'Open           ⌘O',
-            click: function() {
-              return openFile()
-            }
-        }));
+    var text = htmlToText(output.innerHTML);
 
-        file.append(new gui.MenuItem({
-            label: 'Save             ⌘S',
-            click: function() {
-              return saveFile()
-            }
-        }));
-        
-        file.append(new gui.MenuItem({ type: 'separator' }));
-
-        file.append(new gui.MenuItem({
-            label: 'Print',
-            click: function() {
-              return printFile()
-            }
-        }));
-
-        var help = new gui.Menu();
-
-        win.menu = menubar;
-        win.menu.insert(new gui.MenuItem({ label: 'File', submenu: file}), 1);
-        win.menu.append(new gui.MenuItem({ label: 'Help', submenu: help}));
-
-
+    if (document.filePath) {
+      return fs.writeFile(document.filePath, text, function(err) {
+        if (err) throw err;
+      })
     }
 
-    function newFile () {
+    filePicker('saveDialog', function(value){
+      
+      document.filePath = value;
+      document.fileName = filePath.replace(/^.*[\\\/]/, '');
 
-      var win = gui.Window.get();
+      fs.writeFile(document.filePath, text, function(err) {
+        if (err) throw err;
+        currentWindow.title = document.fileName;
+      });
 
-      var newX = win.x;
-          newX += 32;
+    });     
+  }
 
-      var newY = win.y;
-          newY += 32;
+  function readFile () {
 
-      gui.Window.open('index.html?newWindow=true&x=' + newX + '&y=' + newY, windowPrefs)    
+    filePicker('openDialog', function(value){
+      
+      document.filePath = value;
+      document.fileName = document.filePath.replace(/^.*[\\\/]/, '');
+      
+      if (fileIsOpen(document.filePath)) {
+        var openWindow = fileIsOpen(document.filePath);
+            openWindow.focus();
+        return currentWindow.close(true);
+      };
 
+      fs.readFile(document.filePath, 'utf8', function(err, data) {
+
+       if (err) throw err;
+       currentWindow.title = document.fileName;
+
+       input.innerHTML = htmlToText(data);
+       output.innerHTML  = htmlToText(data);
+
+      });
+    })
+
+  }
+
+  function filePicker (id, callback) {
+    var picker = document.getElementById(id);
+
+    picker.addEventListener('change', function(e){
+
+      return callback(this.value)
+
+    }, false);
+
+    picker.click();
+  }
+
+  function fileIsOpen (path) {
+
+    for (var i in global.typewriterWindows) {
+
+      var openWindow = global.typewriterWindows[i],
+          openPath = global.typewriterWindows[i].window.document.filePath;
+
+      if (openWindow === currentWindow) {continue};
+
+      if (openPath === path) {return openWindow};
     }
 
-    function strip(html)
-    {
-       var tmp = window.document.createElement("DIV");
-       tmp.innerHTML = html;
-       return tmp.textContent || tmp.innerText || "";
-    }
+    return false
+  }
 
-   function getHTML (str) {
-      str = '<p>' + str;
-      str = str.replace(/[\n]/gi, "</p>");
-      return str
-   }
+  function buildMenu () {
+     var menubar = new gui.Menu({ type: 'menubar' });
+     var file = new gui.Menu();
+     var win = gui.Window.get();
 
-   function getText () {
-      var str = output.innerHTML;
-      str = str.replace(/<p>/gi, "");
-      str = str.replace(/<\/p>/gi, "\n");
-      str = str.replace(/&nbsp;/gi, "");
-      str = str.replace(/\s<span class="strike">[^>]*<\/span>/g, '');
-      str = str.replace(/<span class="strike">[^>]*<\/span>/g, '');
-      str = str.replace(/<span>/gi, "");
-      str = str.replace(/<\/span>/gi, "");
-      return str
-   }
+     file.append(new gui.MenuItem({
+         label: 'New             ⌘N',
+         click: function() {
+         return newFile()
+         }
+     }));
 
-   function saveFile (callback) {
+     file.append(new gui.MenuItem({
+         label: 'Open           ⌘O',
+         click: function() {
+           return openFile()
+         }
+     }));
 
-      var data = getText();
+     file.append(new gui.MenuItem({
+         label: 'Save             ⌘S',
+         click: function() {
+           return saveFile()
+         }
+     }));
+     
+     file.append(new gui.MenuItem({ type: 'separator' }));
 
-      if (!filePath) {
-         chooseFile('#saveDialog');
-      } else {
-         fs.writeFile(filePath, data, function(err) {
-           if (err) throw err;
+     file.append(new gui.MenuItem({
+         label: 'Print',
+         click: function() {
+           return printFile()
+         }
+     }));
 
-           if (callback) {
-            return callback()
-           }
+     var help = new gui.Menu();
 
-           console.log('writeFile succeeded.');
-         });         
-      }
-
-     function chooseFile(name) {
-         var chooser = document.querySelector(name);
-         chooser.addEventListener("change", function(evt) {
-           
-           var path = this.value;
-
-            fs.writeFile(path, data, function(err) {
-              if (err) throw err;
-              filePath = path;
-              fileName = filePath.replace(/^.*[\\\/]/, '');
-              win = gui.Window.get();
-              win.title = fileName;
-
-              console.log('writeFile succeeded.');
-            });
-
-         }, false);
-
-         chooser.click();  
-       }
-       
-
-   }
+     win.menu = menubar;
+     win.menu.insert(new gui.MenuItem({ label: 'File', submenu: file}), 1);
+     win.menu.append(new gui.MenuItem({ label: 'Help', submenu: help}));
+  }
 
   function getParams () {
     var params = {};
@@ -175,96 +210,47 @@ var app = function () {
     return params
   };
 
-  function openFile () {
-    var win = gui.Window.get();
+  function textToHTML (text) {
+    text = '<p>' + text;
+    text = text.replace(/[\n]/gi, "</p>");
+    return text
+  }
 
-    var newX = win.x;
-        newX += 32;
+  function htmlToText (html) {
+    html = html.replace(/<p>/gi, "");
+    html = html.replace(/<\/p>/gi, "\n");
+    html = html.replace(/&nbsp;/gi, "");
+    html = html.replace(/\s<span class="strike">[^>]*<\/span>/g, '');
+    html = html.replace(/<span class="strike">[^>]*<\/span>/g, '');
+    html = html.replace(/<span>/gi, "");
+    html = html.replace(/<\/span>/gi, "");
+    return html
+  }
 
-    var newY = win.y;
-        newY += 32;
+  return function init () {
 
-      var newWindow = gui.Window.open('index.html?open=true&x=' + newX + '&y=' + newY, windowPrefs);
-  };
+      var params = getParams();
 
-  function readFile () {
+      if (params.firstWindow) {
+        alert('first window');
+        global.typewriterWindows = [currentWindow];
+      } else {
+        global.typewriterWindows.push(currentWindow)
+      }
 
-        chooseFile('#openDialog');
-
-        function chooseFile(name) {
-            var chooser = document.querySelector(name);
-            chooser.addEventListener("change", function(evt) {
-
-              filePath = this.value;
-              fileName = filePath.replace(/^.*[\\\/]/, '');
-              
-              for (var i in global.openFiles) {
-                if (global.openFiles[i] === filePath) {
-                  return confirm( fileName + ' is already open in Typewriter.');
-                }
-              }
-
-              fs.readFile(filePath, 'utf8', function(err, data) {
-
-               if (err) throw err;
-               win = gui.Window.get();
-               win.title = fileName;
-
-               input.innerHTML = getHTML(data);
-               output.innerHTML  = getHTML(data);
-
-               global.openFiles.push(filePath);
-
-              });
-
-            }, false);
-
-            chooser.click();  
-          }
-
-
-    }
-
-    return function init () {
-
-      buildMenu();
-
-      var currentWindow = gui.Window.get();
-
-      // first app window
-      if (!getParams().newWindow && !getParams().open) {
-        global.openFiles = [];
-      } 
-
-      if (getParams().x && getParams().y) {
-        var toX = parseInt(getParams().x);
-        var toY = parseInt(getParams().y);                
-        currentWindow.y = toY;
-        currentWindow.x = toX;
+      if (params.x && params.y) {
+        currentWindow.x = parseInt(params.x);
+        currentWindow.y = parseInt(params.y);
       }
       
       currentWindow.show();
       currentWindow.focus();
 
-      currentWindow.on('close', function(){
-        if (filePath !== undefined || getText() === '') {
-          return this.close(true);  
-        }
-        
-        var reallyClose = window.confirm("You have not yet saved your work. Do you still want to close this document?");
-        if (reallyClose) {
-          this.close(true);          
-        } else {
-          saveFile();
-        }          
-      
-      });
-
-      if (getParams().open) {
+      if (params.openFile) {
         readFile();
       };
 
+      currentWindow.on('close', closeFile);
   
     }();
-
 }
