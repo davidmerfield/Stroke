0c451d2745fd75f0fd3c1e211f1de5793d9ba5ba
David Merfield <dmerfield@gmail.com>
2014-05-08 19:33:20 -0700

    updated paths to ./app

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,12 +7,12 @@ module.exports = function(grunt) {
             credits: "./app/credits.html",
             build_dir: './builds', // Where the build version of my node-webkit app is saved
             mac: true, // We want to build it for mac
-            mac_icns: './src/img/app.icns',
+            mac_icns: './app/Typewriter.icns',
             win: false, // We want to build it for win
             linux32: false, // We don't need linux32
             linux64: false // We don't need linux64
         },
-        src: ['./src/*'] // Your node-webkit app
+        src: ['./app/**/*'] // Your node-webkit app
       },
     })
 
